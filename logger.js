d313b941005026e54b87ac920aef4aacc3ab1161
aknts <25027425+aknts@users.noreply.github.com>
2021-05-02 20:51:35 +0300

    Update logger.js
    Added an if to allow printing only to local console output.

--- logger.js
+++ logger.js
@@ -1,19 +1,21 @@
 // Converted to using mqttmod to dump message to an mqtt topic and at console.
-module.exports = function (broker, logtopic, mqttmod, callback){
+module.exports = function (broker, logtopic, mqttmod, local, callback){
 	var module = {};
 	module.log = function(level, message, broker, logtopic, mqttmod, callback){
 		var msg = '['+(new Date())+'] [' + level + '] ['+ message + ']';
-		mqttmod.send(broker, logtopic, msg, callback);
+		if (local == 0) {
+			mqttmod.send(broker, logtopic, msg, callback);
+		}
 		console.log('['+(new Date())+'] [' + level + '] ['+ message + ']');
 	}
 	module.error = function(message){
-		this.log('ERROR', message, broker, logtopic, mqttmod, callback);
+		this.log('ERROR', message, broker, logtopic, mqttmod, local, callback);
 	}
 	module.info = function(message){
-		this.log('INFO', message, broker, logtopic, mqttmod, callback);
+		this.log('INFO', message, broker, logtopic, mqttmod, local, callback);
 	}
 	module.debug = function(message){
-		this.log('DEBUG', message, broker, logtopic, mqttmod, callback);
+		this.log('DEBUG', message, broker, logtopic, mqttmod, local, callback);
 	}
 	return module;
 };
